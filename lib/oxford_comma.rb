d6f39818d7212aaa5494cf2903a112d3cd6b04ee
Christopher Tero <christopher.tero@gmail.com>
2019-03-30 21:47:55 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[2] = "and " + array[2]
+    array.join(", ")
+  else
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end
 end
